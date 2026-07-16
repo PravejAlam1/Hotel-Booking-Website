62c013a9fa073df0675a321a653dc38132f5241b
PravejAlam1 <collegepravej@gmail.com>
2026-07-16 20:30:07 +0530

    Setup backend with Express and MongoDB Atlas connection

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("StaySphre Backend is running Successfully........");
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
